e7c77af4c293451c16b92c597cd92fb9a7ab2364
Evgeniy <eugenr@clout.com>
2016-10-03 16:54:56 +0300

    build jsx live compiler

--- App.js
+++ App.js
@@ -2,51 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			data: [
-			{id: 1, name: "Simon Baicvxley"},
-			{id: 2, name: "sdsdf Baisdfsley"},
-			{id: 3, name: "Svbzxcimon Baizxcvley"},
-			{id: 4, name: "Simon Bazxcviley"},
-			{id: 5, name: "Sixcvzxcmon Bailey"},
-			{id: 6, name: "Simoxcn Baixcvley"},
-			{id: 7, name: "Simon Bailexcvzy"},
-			{id: 8, name: "Sxcvimon Bailey"},
-			{id: 9, name: "Simzxcvxcvon Baixvcley"},
-			{id: 10, name: "Sivxcmon Bailey"},
-			{id: 11, name: "Sizxcvmon Bailey"},
-			{id: 12, name: "Simon Bailxcvey"},
-			{id: 13, name: "Simxcvon Baivxcvley"},
-			{id: 14, name: "Simon Bailey"},
-			{id: 15, name: "Sixcvmon xcvBailey"},
-			{id: 16, name: "Simon Bailey"},
-			{id: 17, name: "Sixcvmon Bailey"},
-			{id: 18, name: "Simon Bailxcvey"}
-			]
-		}
-	}
-	render() {
-		let rows = this.state.data.map(function(person) {
-			return <PersonRow key={person.id} data={person} />
-		});
-		return (
-			<table>
-			<tbody>{rows}</tbody>
-			</table>
-			);
-	}
+    constructor() {
+        super();
+        this.state = {
+        	input: '/* add your jsx here */',
+        	output: '',
+        	err: ''
+        }
+        this.update = this.update.bind(this);
+    }
+    update(e){
+    	let code = e.target.value;
+    	try {
+    		this.setState({
+    			output: babel.transform(code, {
+    				stage: 0,
+    				loose: 'all'
+    			}).code,
+    			err: ''
+    		})
+    	}
+    	catch(err) {
+    		this.setState({
+    			err: err.message
+    		})
+    	}
+    }
+    render() {
+        return (
+        	<div>
+        		<header>{this.state.err}</header>
+        		<div className="container">
+        			<textarea
+        			onChange={this.update}
+        			defaultValue={this.state.input}>
+        			</textarea>
+        			<pre>
+        				{this.state.output}
+        			</pre>
+        		</div>
+        	</div>
+        	);
+    }
 }
 
-
-function PersonRow(props) {
-	return (
-		<tr>
-		<td>{props.data.id}</td>
-		<td>{props.data.name}</td>
-		</tr>
-		);
-}
 
 export default App;
